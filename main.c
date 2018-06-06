1891db12cfb4593402808227bd3e9912a0cfe001
Ivan Sanin <monstruos@mail.ru>
2018-06-06 10:57:42 +0700

    Добавлено окончательное решение и .gitignore

--- main.c
+++ main.c
@@ -1,20 +1,31 @@
+#include <stdlib.h>
 #include <stdio.h>
 
 
 int main(int argc, char const *argv[])
 {
-    int i, j, input;
+    int it = 0, j, num, input;
 
     scanf("%i", &input);
     int *output = (int *) malloc(sizeof(int) * input);
 
-    for(i = 0; i < input; ++i) {
-        output[i] = 1;
-        for(j = 0; j < i; ++j) {
-            output[i] *= output[j];
+    output[it] = 2;
+    printf("%i ", output[it]);
+
+    for (it++, num = 3; it < input; num += 2) {
+        int is_prime = 1;
+        for (j = 3; j < num / 2; j += 2) {
+            if (num % j == 0) {
+                is_prime = 0;
+                break;
+            }
         }
-        output[i]++;
-        printf("%i ", output[i]);
+
+        if (is_prime == 1) {
+            output[it] = num;
+            printf("%i ", output[it]);
+            it++;
+        }
     }
     printf("\n");
 
